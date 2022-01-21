df71870a5ce1934253d1bf498586e30bd1773d45
PeculiarProgrammer <marcuwynu23@gmail.com>
2022-01-21 12:13:25 +0800

    .

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
- 
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+        return render_template("/features/home.html")
+
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True,host='0.0.0.0',port=9000)
